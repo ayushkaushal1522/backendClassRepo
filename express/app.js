29177e6c57ac112b68ba8e7dec97809d1ef677ed
ayushkaushal1522 <ayushkaushal1522@gmail.com>
2023-08-24 15:52:22 +0530

    commit the change

---
+++ express/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+
+// console.log(app);
+
+app.use((req , res)=>{
+    console.log("Hello Everyone");
+    console.log(req);
+    res.send("Ayush kaushal is an industrious boy")
+})
+
+
+
+app.listen(8080 ,()=>{
+    console.log("Server connected succesfully at port 8080");
+})
